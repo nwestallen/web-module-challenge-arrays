99f7cbe4542fd6597c78090e74bf5d18c34be3a0
Nick Allen <nwestallen@gmail.com>
2020-12-08 21:47:02 -0500

    Completed tasks 4 & 5

--- index.js
+++ index.js
@@ -103,8 +103,9 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
+function removeLastFlavor(myArray){
+   myArray.pop();
+   return myArray;
 }
 
 
@@ -120,8 +121,8 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(myArray,num){
+    return myArray[num];
 }
 
 
